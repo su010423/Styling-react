43082c006040826b74b14403aabdfbda291dc5c8
Soo Hong Lee <crux2476@gmail.com>
2022-08-09 21:13:58 +0900

    9.4.5 반응형 디자인

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -516,9 +516,15 @@ module.exports = function (webpackEnv) {
                   modules: {
                     mode: 'icss',
                   },
-                },
-                'sass-loader'
-              ),
+                }).concat({
+                  loader: require.resolve('sass-loader'),
+                  options: {
+                    sassOptions: {
+                      includePaths: [paths.appSrc + '/styles'],
+                    },
+                    additionalData: `@import 'utils';`
+                  }
+                }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
               // Remove this when webpack adds a warning or an error for this.
